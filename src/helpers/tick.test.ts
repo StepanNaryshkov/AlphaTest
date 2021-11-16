e0435a145679da982576afcb2a0b4c3a90bf0c8c
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-11-16 17:43:28 +0200

    added test case for tick

--- src/helpers/tick.test.ts
+++ src/helpers/tick.test.ts
@@ -18,4 +18,22 @@ describe('tick', () => {
     };
     expect(tick(data)).toStrictEqual(expectedData())
   });
+
+  test('should be the same result after tick', () => {
+    data[3][5] = 1;
+    data[3][4] = 1;
+    data[4][5] = 1;
+    data[4][4] = 1;
+
+    const expectedData = () => {
+      const res = getEmptyMap();
+      res[3][5] = 1;
+      res[3][4] = 1;
+      res[4][5] = 1;
+      res[4][4] = 1;
+
+      return res;
+    };
+    expect(tick(data)).toStrictEqual(expectedData())
+  });
 });
